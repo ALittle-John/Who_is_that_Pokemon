a7d02bb568fcba229ad66c497badaee2a544ddab
ALittle-John <joaopsousaqi@gmail.com>
2023-12-21 13:46:33 -0300

    refactor: updating html structure and start js logic

--- src/frontend/pages/index.js
+++ src/frontend/pages/index.js
@@ -1,3 +1,18 @@
-function startPage() {}
+class IndexPage {
+  constructor() {
+    this.mySVG = document.getElementById('toggleMenu');
+    this.optionsMenu = document.querySelectorAll('.navgation a');
 
-export default startPage;
+    this.mySVG.addEventListener('click', this.toggleMenu.bind(this));
+  }
+
+  toggleMenu() {
+    this.optionsMenu.forEach((option) => {
+      option.classList.toggle('show');
+      option.classList.toggle('hidden');
+    });
+  }
+}
+
+// menu.toggleMenu();
+const menu = new IndexPage();
