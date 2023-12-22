f2f0a938b17ef2a88422ae0ed656c27b94f2db11
ALittle-John <joaopsousaqi@gmail.com>
2023-12-22 11:30:15 -0300

    rearranging file and folders and preeparing to docker and axios

--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -1,13 +1,10 @@
-import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-//Initial page route
-//Send to other pages
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const routes = (app) => {
-  const frontEndApp = path.join(__dirname, '../../frontend/pages');
+  const frontEndApp = path.join(__dirname, '../../frontend');
 
   app.route('/').get((req, res) => {
     res.status(200).sendFile(path.join(frontEndApp, 'index.html'));
